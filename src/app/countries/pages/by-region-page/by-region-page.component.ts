88b258fe425a8c26040d56e31eb886c4fa01e3c3
Manuel Alvarado <manuel.alvarado@bcr.gob.sv>
2024-02-02 11:15:28 -0600

    implementando Type Region in PageByRegion

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,19 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
   styleUrls: ['./by-region-page.component.css'],
 })
 export class ByRegionPageComponent implements OnInit {
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public regionSelected?: Region;
   public countries: Country[] = [];
   constructor(private countriesService: CountriesService) {}
 
   ngOnInit(): void {}
 
-  searchByValue(value: string): void {
-    this.countriesService.searchByRegion(value).subscribe((res) => {
+  searchByValue(region: Region): void {
+    this.regionSelected = region;
+    this.countriesService.searchByRegion(region).subscribe((res) => {
       this.countries = res;
     });
   }
